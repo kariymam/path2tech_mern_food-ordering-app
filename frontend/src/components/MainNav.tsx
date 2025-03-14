2844f39b9bfb92ac9538c9884888f020cf208edf
kariymam <59487728+kariymam@users.noreply.github.com>
2025-03-14 13:02:24 -0400

    Authenticated login Add Auth0 support to start on authenticated experience on the frontend

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { UserRound } from "lucide-react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MainNav = () => {
+    const { loginWithRedirect } = useAuth0()
     return (
         <nav id="main_navigation" className="flex items-center gap-3">
-            <Button variant="outline" className="max-w-9 md:max-w-fit"><Link to="/login">
-                <UserRound className="md:hidden" />
-                <span className="hidden md:block">Log in</span></Link></Button>
+            <Button
+                variant="outline"
+                className="max-w-9 md:max-w-fit"
+                onClick={async () => await loginWithRedirect()}>
+                    <Link to="/login">
+                    <UserRound className="md:hidden" />
+                    <span className="hidden md:block">Log in</span></Link></Button>
             <Button><Link to="/sign-up">Sign up</Link></Button>
         </nav>
     )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <AppRoutes/>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes/>
+      </Auth0ProviderWithNavigate>
     </Router>
   </StrictMode>,
 )
