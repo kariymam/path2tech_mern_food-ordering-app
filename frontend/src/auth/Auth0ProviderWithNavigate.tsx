0d95fedc937755a11b83feff01139551a839324e
kariymam <59487728+kariymam@users.noreply.github.com>
2025-03-26 14:53:41 -0400

    Security updates: Auth0 Access Token & validate access token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -10,8 +10,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if (!domain || !clientId || !redirectUri) {
+    if (!domain || !clientId || !redirectUri || !audience) {
         throw new Error("unable to initialize auth")
     }
 
@@ -23,7 +24,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         <Auth0Provider 
         domain={domain} 
         clientId={clientId} 
-        authorizationParams={{ redirect_uri: redirectUri }}
+        authorizationParams={{ 
+            redirect_uri: redirectUri, 
+            audience,
+        }}
         onRedirectCallback={onRedirectCallback}>
             {children}
         </Auth0Provider>
